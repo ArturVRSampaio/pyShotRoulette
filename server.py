cd8b2ba536d8ce9c2107cd85708c3ea318670082
RodolphoVSantoro <rodolphovs@gmail.com>
2024-05-12 08:55:43 -0300

    remove dead code

--- server.py
+++ server.py
@@ -15,7 +15,6 @@ def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.settimeout(2)
-    # server_socket.setblocking(True)
     server_socket.bind((host, port))
     server_socket.listen(10)
 
