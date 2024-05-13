aa6b416a44252539710aa2c2c14156ff17b59c01
RodolphoVSantoro <rodolphovs@gmail.com>
2024-05-13 14:20:14 -0300

    fix protocol to wait on slow connections

--- mtp.py
+++ mtp.py
@@ -12,5 +12,11 @@ def send_message(connection: socket, message_string: str) -> None:
 
 
 def get_message(connection: socket) -> str:
-    message_size = int.from_bytes(connection.recv(4), byteorder=BYTE_ORDER)
-    return connection.recv(message_size).decode(ENCODING)
+    size_data = connection.recv(4)
+    while len(size_data) < 4:
+        size_data += connection.recv(4 - len(size_data))
+    message_size = int.from_bytes(size_data, byteorder=BYTE_ORDER)
+    message_data = connection.recv(message_size)
+    while len(message_data) < message_size:
+        message_data += connection.recv(message_size - len(message_data))
+    return message_data.decode(ENCODING)
